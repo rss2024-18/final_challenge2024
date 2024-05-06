04093551fc0fcef978f8eeb49a5a88ba85b6f64d
doraxhu <dorah@mit.edu>
2024-05-06 20:15:55 +0000

    added package for luigi

---
+++ city_driving/path_planner/utils.py
@@ -0,0 +1,378 @@
+import rclpy
+
+import numpy as np
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Pose, PoseArray, Point
+from std_msgs.msg import Header
+import os
+from typing import List, Tuple
+import json
+from tf_transformations import euler_from_quaternion
+from collections import deque
+import math
+from numpy.linalg import inv
+from scipy.ndimage import binary_dilation
+
+EPSILON = 0.00000000001
+
+''' These data structures can be used in the search function
+'''
+
+
+class LineTrajectory:
+    """ A class to wrap and work with piecewise linear trajectories. """
+
+    def __init__(self, node, viz_namespace=None):
+        self.points: List[Tuple[float, float]] = []
+        self.distances = []
+        self.has_acceleration = False
+        self.visualize = False
+        self.viz_namespace = viz_namespace
+        self.node = node
+
+        if viz_namespace:
+            self.visualize = True
+            self.start_pub = self.node.create_publisher(Marker, viz_namespace + "/start_point", 1)
+            self.traj_pub = self.node.create_publisher(Marker, viz_namespace + "/path", 1)
+            self.end_pub = self.node.create_publisher(Marker, viz_namespace + "/end_pose", 1)
+
+    # compute the distances along the path for all path segments beyond those already computed
+    def update_distances(self):
+        num_distances = len(self.distances)
+        num_points = len(self.points)
+
+        for i in range(num_distances, num_points):
+            if i == 0:
+                self.distances.append(0)
+            else:
+                p0 = self.points[i - 1]
+                p1 = self.points[i]
+                delta = np.array([p0[0] - p1[0], p0[1] - p1[1]])
+                self.distances.append(self.distances[i - 1] + np.linalg.norm(delta))
+
+    def distance_to_end(self, t):
+        if not len(self.points) == len(self.distances):
+            print(
+                "WARNING: Different number of distances and points, this should never happen! Expect incorrect results. See LineTrajectory class.")
+        dat = self.distance_along_trajectory(t)
+        if dat == None:
+            return None
+        else:
+            return self.distances[-1] - dat
+
+    def distance_along_trajectory(self, t):
+        # compute distance along path
+        # ensure path boundaries are respected
+        if t < 0 or t > len(self.points) - 1.0:
+            return None
+        i = int(t)  # which segment
+        t = t % 1.0  # how far along segment
+        if t < EPSILON:
+            return self.distances[i]
+        else:
+            return (1.0 - t) * self.distances[i] + t * self.distances[i + 1]
+
+    def addPoint(self, point: Tuple[float, float]) -> None:
+        print("adding point to trajectory:", point)
+        self.points.append(point)
+        self.update_distances()
+        self.mark_dirty()
+
+    def clear(self):
+        self.points = []
+        self.distances = []
+        self.mark_dirty()
+
+    def empty(self):
+        return len(self.points) == 0
+
+    def save(self, path):
+        print("Saving trajectory to:", path)
+        data = {}
+        data["points"] = []
+        for p in self.points:
+            data["points"].append({"x": p[0], "y": p[1]})
+        with open(path, 'w') as outfile:
+            json.dump(data, outfile)
+
+    def mark_dirty(self):
+        self.has_acceleration = False
+
+    def dirty(self):
+        return not self.has_acceleration
+
+    def load(self, path):
+        print("Loading trajectory:", path)
+
+        # resolve all env variables in path
+        path = os.path.expandvars(path)
+
+        with open(path) as json_file:
+            json_data = json.load(json_file)
+            for p in json_data["points"]:
+                self.points.append((p["x"], p["y"]))
+        self.update_distances()
+        print("Loaded:", len(self.points), "points")
+        self.mark_dirty()
+
+    # build a trajectory class instance from a trajectory message
+    def fromPoseArray(self, trajMsg):
+        for p in trajMsg.poses:
+            self.points.append((p.position.x, p.position.y))
+        self.update_distances()
+        self.mark_dirty()
+        print("Loaded new trajectory with:", len(self.points), "points")
+
+    def toPoseArray(self):
+        traj = PoseArray()
+        traj.header = self.make_header("/map")
+        for i in range(len(self.points)):
+            p = self.points[i]
+            pose = Pose()
+            pose.position.x = p[0]
+            pose.position.y = p[1]
+            traj.poses.append(pose)
+        return traj
+
+    def publish_start_point(self, duration=0.0, scale=0.1):
+        should_publish = len(self.points) > 0
+        #self.node.get_logger().info("Before Publishing start point")
+        if self.visualize and self.start_pub.get_subscription_count() > 0:
+            #self.node.get_logger().info("Publishing start point")
+            marker = Marker()
+            marker.header = self.make_header("/map")
+            marker.ns = self.viz_namespace + "/trajectory"
+            marker.id = 0
+            marker.type = 2  # sphere
+            marker.lifetime = rclpy.duration.Duration(seconds=duration).to_msg()
+            if should_publish:
+                marker.action = 0
+                marker.pose.position.x = self.points[0][0]
+                marker.pose.position.y = self.points[0][1]
+                marker.pose.orientation.w = 1.0
+                marker.scale.x = 1.0
+                marker.scale.y = 1.0
+                marker.scale.z = 1.0
+                marker.color.r = 0.0
+                marker.color.g = 1.0
+                marker.color.b = 0.0
+                marker.color.a = 1.0
+            else:
+                # delete marker
+                marker.action = 2
+
+            self.start_pub.publish(marker)
+        elif self.start_pub.get_subscription_count() == 0:
+            self.node.get_logger().info("Not publishing start point, no subscribers")
+
+    def publish_end_point(self, duration=0.0):
+        should_publish = len(self.points) > 1
+        if self.visualize and self.end_pub.get_subscription_count() > 0:
+            marker = Marker()
+            marker.header = self.make_header("/map")
+            marker.ns = self.viz_namespace + "/trajectory"
+            marker.id = 1
+            marker.type = 2  # sphere
+            marker.lifetime = rclpy.duration.Duration(seconds=duration).to_msg()
+            if should_publish:
+                marker.action = 0
+                marker.pose.position.x = self.points[-1][0]
+                marker.pose.position.y = self.points[-1][1]
+                marker.pose.orientation.w = 1.0
+                marker.scale.x = 1.0
+                marker.scale.y = 1.0
+                marker.scale.z = 1.0
+                marker.color.r = 1.0
+                marker.color.g = 0.0
+                marker.color.b = 0.0
+                marker.color.a = 1.0
+            else:
+                # delete marker
+                marker.action = 2
+
+            self.end_pub.publish(marker)
+        elif self.end_pub.get_subscription_count() == 0:
+            print("Not publishing end point, no subscribers")
+
+    def publish_trajectory(self, duration=0.0):
+        should_publish = len(self.points) > 1
+        if self.visualize and self.traj_pub.get_subscription_count() > 0:
+            #self.node.get_logger().info("Publishing trajectory")
+            marker = Marker()
+            marker.header = self.make_header("/map")
+            marker.ns = self.viz_namespace + "/trajectory"
+            marker.id = 2
+            marker.type = marker.LINE_STRIP  # line strip
+            marker.lifetime = rclpy.duration.Duration(seconds=duration).to_msg()
+            if should_publish:
+                marker.action = marker.ADD
+                marker.scale.x = 0.3
+                marker.color.r = 1.0
+                marker.color.g = 1.0
+                marker.color.b = 1.0
+                marker.color.a = 1.0
+                for p in self.points:
+                    pt = Point()
+                    pt.x = p[0]
+                    pt.y = p[1]
+                    pt.z = 0.0
+                    marker.points.append(pt)
+            else:
+                # delete
+                marker.action = marker.DELETE
+            self.traj_pub.publish(marker)
+            print('publishing traj')
+        elif self.traj_pub.get_subscription_count() == 0:
+            print("Not publishing trajectory, no subscribers")
+
+    def publish_viz(self, duration=0):
+        if not self.visualize:
+            print("Cannot visualize path, not initialized with visualization enabled")
+            return
+        self.publish_start_point(duration=duration)
+        self.publish_trajectory(duration=duration)
+        self.publish_end_point(duration=duration)
+
+    def make_header(self, frame_id, stamp=None):
+        if stamp == None:
+            stamp = self.node.get_clock().now().to_msg()
+        header = Header()
+        header.stamp = stamp
+        header.frame_id = frame_id
+        return header
+
+# class Coordinate2D:
+#     """
+#     2D coordinate object used in Map representation
+#     """
+#     def __init__(self, x: float=0.0, y: float=0.0):
+#         self.x = x
+#         self.y = y
+
+#     def __str__(self):
+#         return f"({self.x}, {self.y})"
+
+#     def __eq__(self, other):
+#         return self.x == other.x and self.y == other.y
+    
+#     def scalar_multiply(self, scalar):
+#         return Coordinate2D(self.x * scalar, self.y * scalar)
+
+class Map():
+    """
+    2D map discretization Abstract Data Type
+    """
+    def __init__(self, msg, node) -> None:
+        self.node = node
+        self.height = msg.info.height #1300
+        self.width = msg.info.width #1730
+        self.resolution = msg.info.resolution
+        self.orientation = msg.info.origin.orientation
+        poseOrientation = [self.orientation.x, self.orientation.y, self.orientation.z, self.orientation.w]
+        self.angles = euler_from_quaternion(poseOrientation)
+        self.posePoint = np.array([[msg.info.origin.position.x], [msg.info.origin.position.y], [msg.info.origin.position.z]])
+        self.map_translation = (msg.info.origin.position.x, msg.info.origin.position.y)
+        self.data = np.reshape(np.array(msg.data), (self.height, self.width))
+        self.raw_data = np.array(msg.data).shape
+        self.transformation_matrix = None
+
+    def dilate_map(self):
+        struct_element = np.ones((11, 11))
+        dilated_map = binary_dilation(self.data, structure=struct_element).astype(np.uint8)
+        return dilated_map
+
+    def z_axis_rotation_matrix(self, yaw):
+        return np.array([[np.cos(yaw), -np.sin(yaw), 0], [np.sin(yaw), np.cos(yaw), 0], [0, 0, 1]])
+    
+    def transformation_matrix_p2r(self):
+        rotation_matrix = self.z_axis_rotation_matrix(self.angles[2])
+        rotation_matrix[0][2] = self.map_translation[0]
+        rotation_matrix[1][2] = self.map_translation[1]
+        diagonal_values = [self.resolution, self.resolution, 1]
+        scaling_matrix = np.diag(diagonal_values)
+        return np.matmul(rotation_matrix, scaling_matrix)
+
+    def pixel_to_real(self, pixelCoord: Tuple[int, int]) -> Tuple[float, float]:
+        self.transformation_matrix = self.transformation_matrix_p2r()
+        new_pcoord = (pixelCoord[0], pixelCoord[1], 1.0)
+        coords = np.matmul(self.transformation_matrix, new_pcoord)
+        # self.node.get_logger().info("real_value: " + str(self.discretization(coords[0], coords[1])))
+        return self.discretization(coords[0], coords[1])
+
+    def real_to_pixel(self, realCoord: Tuple[float, float]) -> Tuple[int, int]:
+        self.transformation_matrix = self.transformation_matrix_p2r()
+        new_rcoord = (realCoord[0], realCoord[1], 1.0)
+        x = np.matmul(inv(self.transformation_matrix), new_rcoord)
+        # self.node.get_logger().info("pixel_value: " + str(x.astype(int)))
+        return x.astype(int)
+    
+    def get_pixel(self, u, v):
+        return self.data[v][u]
+
+    def discretization(self, x, y):
+        return (math.floor(x) + 0.5, math.floor(y) + 0.5)
+
+    def get_neighbors(self, x, y):
+        neighbors = []
+        directions = [(-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0)]
+
+        for dx, dy in directions:
+            # nx, ny = x + dx/2, y + dy/2
+            nx, ny = x + dx/2, y + dy/2
+            # self.node.get_logger().info("NX, NY: " + str((nx, ny)))
+            u, v, b = self.real_to_pixel((nx, ny))
+            # self.node.get_logger().info("U, V: " + str((u, v)))
+            # self.node.get_logger().info("Free" if self.get_pixel(u, v) == 0 else "Occupied")
+            # if True:
+            if self.get_pixel(u, v) == 0:
+                move_cost = 14 if dx != 0 and dy != 0 else 10  # Diagonal moves cost 14, others cost 10
+                neighbors.append((nx, ny, move_cost))
+        return neighbors
+    
+    def heuristic(self, a, b):
+        (x1, y1) = a
+        (x2, y2) = b
+        return math.sqrt((x1 - x2)**2 + (y1 - y2)**2) * 10
+    
+    def a_star(self, start, end):
+        self.data = self.dilate_map()
+        start = self.discretization(start[0], start[1])
+        end = self.discretization(end[0], end[1])
+
+        #start = self.discretization(11.094404220581055, -1.1191177368164062)
+        #end = self.discretization(-16.592947006225586, 25.76806640625)
+
+        frontier = [(0, start)]
+        came_from = {start: None}
+        cost_so_far = {start: 0}
+
+        while frontier:
+        # Sort and pop the lowest cost item
+            frontier.sort(key=lambda x: x[0])
+            current = frontier.pop(0)[1]
+
+            if current == end:
+                break
+
+            # Correctly unpack all three values returned from get_neighbors
+            # self.node.get_logger().info("Neighbors: " + str(self.get_neighbors(current[0], current[1])))
+            for nx, ny, move_cost in self.get_neighbors(current[0], current[1]):
+                next_pos = (nx, ny)
+                new_cost = cost_so_far[current] + move_cost
+                if next_pos not in cost_so_far or new_cost < cost_so_far[next_pos]:
+                    cost_so_far[next_pos] = new_cost
+                    priority = new_cost + self.heuristic(end, next_pos)
+                    frontier.append((priority, next_pos))
+                    came_from[next_pos] = current
+
+        return self.reconstruct_path(came_from, start, end)
+
+    def reconstruct_path(self, came_from, start, end):
+        current = end
+        path = []
+        while current != start:
+            path.append(current)
+            current = came_from.get(current)
+        path.append(start)
+        path.reverse()
+        return path
---
+++ luigi_castle/luigi_castle/path_builder.py
@@ -0,0 +1,112 @@
+import rclpy
+from rclpy.node import Node
+
+assert rclpy
+from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped, PoseArray
+from nav_msgs.msg import OccupancyGrid
+from .utils import LineTrajectory, Map
+from std_msgs.msg import int32, Int32MultiArray
+
+import time
+
+import numpy as np
+
+class PathPlan(Node):
+    """ Listens for 3 poses (published from Rviz by TAs clicking) and plots a path that passes each one without going backwards
+    (aka it goes through the Luigi's Basement course)"""
+
+    def __init__(self):
+        super().__init__("TODO") # TODO: change to new name
+        self.declare_parameter('odom_topic', "default")
+        self.declare_parameter('map_topic', "default")
+        self.declare_parameter('initial_pose_topic', "default")
+
+        self.odom_topic = self.get_parameter('odom_topic').get_parameter_value().string_value
+        self.map_topic = self.get_parameter('map_topic').get_parameter_value().string_value
+        self.initial_pose_topic = self.get_parameter('initial_pose_topic').get_parameter_value().string_value
+
+        self.map_sub = self.create_subscription(
+            OccupancyGrid,
+            self.map_topic,
+            self.map_cb,
+            1)
+
+        self.stops_sub = self.create_subscription(
+            PoseStamped,
+            "/clicked_point",
+            self.stop_cb,
+            10
+        )
+
+        self.traj_pub = self.create_publisher(
+            PoseArray,
+            "/trajectory/current",
+            10
+        )
+
+        self.pose_sub = self.create_subscription(
+            PoseWithCovarianceStamped,
+            self.initial_pose_topic,
+            self.pose_cb,
+            10
+        )
+
+        self.index_sub = self.create_subscription(int32, "/index", self.index_cb, 10)
+        self.index_pub = self.create_publisher(Int32MultiArray, "")
+
+        self.trajectory = LineTrajectory(node=self, viz_namespace="/planned_trajectory")
+        self.start = None
+        self.stops = []
+        self.map = None
+    
+    def map_cb(self, msg):
+        self.map = Map(msg, self)
+        
+    def pose_cb(self, msg):
+        self.start = (msg.pose.pose.position.x, msg.pose.pose.position.y)
+
+    def index_cb(self, msg):
+        self.index = msg.data
+
+    def stop_cb(self, msg):
+        if len(self.stops < 3):
+            pose = (msg.pose.position.x, msg.pose.position.y)
+            self.stops.append(pose)
+        else:
+            if self.map is not None and self.start is not None:
+                path = self.plan_path(self.start, self.stops[0], self.stops[1], self.stops[2], self.map)
+                pass
+            
+
+        pass
+        #self.end = (msg.pose.position.x, msg.pose.position.y)
+        # self.get_logger().info("End Pose: " + str(self.end))
+        #if self.map is not None and self.start is not None:
+            #path = self.plan_path(self.start, self.end, self.map)
+    
+
+    def plan_path(self, start_point, stop_1, stop_2, stop_3, map):
+        # construct a trajectory
+        path0 = self.map.a_star(self.start, self.stops[0])
+        path1 = self.map.a_star(self.stops[0], self.stops[1])
+        path2 = self.map.a_star(self.stops[1], self.stops[2])
+
+        # TODO: how make stop at stops?
+        path = path0 + path1 + path2
+        
+        # add to trajectory
+        self.trajectory.clear()
+        if path is not None and len(path) > 0:
+            for point in path:
+                self.trajectory.addPoint(point)
+        else:
+            return
+        self.traj_pub.publish(self.trajectory.toPoseArray())
+        self.trajectory.publish_viz()
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    planner = PathPlan()
+    rclpy.spin(planner)
+    rclpy.shutdown()
