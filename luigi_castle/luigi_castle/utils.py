241e055cde8826375981b091f4a110f79202bfa0
JesusDiaz <79178719+pepeseus@users.noreply.github.com>
2024-05-06 21:36:39 -0400

    Update utils.py

--- luigi_castle/luigi_castle/utils.py
+++ luigi_castle/luigi_castle/utils.py
@@ -8,10 +8,8 @@ import os
 from typing import List, Tuple
 import json
 from tf_transformations import euler_from_quaternion
-from collections import deque
+from scipy.spatial.transform import Rotation as R
 import math
-from numpy.linalg import inv
-from scipy.ndimage import binary_dilation
 
 EPSILON = 0.00000000001
 
@@ -73,7 +71,7 @@ class LineTrajectory:
             return (1.0 - t) * self.distances[i] + t * self.distances[i + 1]
 
     def addPoint(self, point: Tuple[float, float]) -> None:
-        print("adding point to trajectory:", point)
+        #print("adding point to trajectory:", point)
         self.points.append(point)
         self.update_distances()
         self.mark_dirty()
@@ -136,9 +134,9 @@ class LineTrajectory:
 
     def publish_start_point(self, duration=0.0, scale=0.1):
         should_publish = len(self.points) > 0
-        #self.node.get_logger().info("Before Publishing start point")
+        self.node.get_logger().info("Before Publishing start point")
         if self.visualize and self.start_pub.get_subscription_count() > 0:
-            #self.node.get_logger().info("Publishing start point")
+            self.node.get_logger().info("Publishing start point")
             marker = Marker()
             marker.header = self.make_header("/map")
             marker.ns = self.viz_namespace + "/trajectory"
@@ -197,7 +195,7 @@ class LineTrajectory:
     def publish_trajectory(self, duration=0.0):
         should_publish = len(self.points) > 1
         if self.visualize and self.traj_pub.get_subscription_count() > 0:
-            #self.node.get_logger().info("Publishing trajectory")
+            self.node.get_logger().info("Publishing trajectory")
             marker = Marker()
             marker.header = self.make_header("/map")
             marker.ns = self.viz_namespace + "/trajectory"
@@ -241,138 +239,267 @@ class LineTrajectory:
         header.frame_id = frame_id
         return header
 
-# class Coordinate2D:
-#     """
-#     2D coordinate object used in Map representation
-#     """
-#     def __init__(self, x: float=0.0, y: float=0.0):
-#         self.x = x
-#         self.y = y
-
-#     def __str__(self):
-#         return f"({self.x}, {self.y})"
-
-#     def __eq__(self, other):
-#         return self.x == other.x and self.y == other.y
-    
-#     def scalar_multiply(self, scalar):
-#         return Coordinate2D(self.x * scalar, self.y * scalar)
-
-class Map():
+class Map:
     """
     2D map discretization Abstract Data Type
     """
-    def __init__(self, msg, node) -> None:
+    def __init__(self, msg, node, lanes) -> None:
+        
         self.node = node
-        self.height = msg.info.height #1300
-        self.width = msg.info.width #1730
+        self.lanes = lanes
+        self.height = msg.info.height
+        self.width = msg.info.width
         self.resolution = msg.info.resolution
-        self.orientation = msg.info.origin.orientation
-        poseOrientation = [self.orientation.x, self.orientation.y, self.orientation.z, self.orientation.w]
-        self.angles = euler_from_quaternion(poseOrientation)
+        orientation = msg.info.origin.orientation
+        self.poseOrientation = [orientation.x, orientation.y, orientation.z, orientation.w]
+        self.angles = euler_from_quaternion(self.poseOrientation)
         self.posePoint = np.array([[msg.info.origin.position.x], [msg.info.origin.position.y], [msg.info.origin.position.z]])
-        self.map_translation = (msg.info.origin.position.x, msg.info.origin.position.y)
-        self.data = np.reshape(np.array(msg.data), (self.height, self.width))
-        self.raw_data = np.array(msg.data).shape
-        self.transformation_matrix = None
-
-    def dilate_map(self):
-        struct_element = np.ones((11, 11))
-        dilated_map = binary_dilation(self.data, structure=struct_element).astype(np.uint8)
-        return dilated_map
+        self.data = np.array(msg.data).reshape((self.height, self.width))
 
     def z_axis_rotation_matrix(self, yaw):
         return np.array([[np.cos(yaw), -np.sin(yaw), 0], [np.sin(yaw), np.cos(yaw), 0], [0, 0, 1]])
-    
-    def transformation_matrix_p2r(self):
-        rotation_matrix = self.z_axis_rotation_matrix(self.angles[2])
-        rotation_matrix[0][2] = self.map_translation[0]
-        rotation_matrix[1][2] = self.map_translation[1]
-        diagonal_values = [self.resolution, self.resolution, 1]
-        scaling_matrix = np.diag(diagonal_values)
-        return np.matmul(rotation_matrix, scaling_matrix)
 
     def pixel_to_real(self, pixelCoord: Tuple[int, int]) -> Tuple[float, float]:
-        self.transformation_matrix = self.transformation_matrix_p2r()
-        new_pcoord = (pixelCoord[0], pixelCoord[1], 1.0)
-        coords = np.matmul(self.transformation_matrix, new_pcoord)
-        # self.node.get_logger().info("real_value: " + str(self.discretization(coords[0], coords[1])))
-        return self.discretization(coords[0], coords[1])
+        pixelCoord = [i*self.resolution for i in pixelCoord]
+        rotatedCoord = np.array([pixelCoord[0]], [pixelCoord[1]], [0.0]) * self.z_axis_rotation_matrix(self.angles[2])
+        rotatedCoord = rotatedCoord + self.posePoint
+        return (rotatedCoord[0, 0], rotatedCoord[1, 0])      
 
     def real_to_pixel(self, realCoord: Tuple[float, float]) -> Tuple[int, int]:
-        self.transformation_matrix = self.transformation_matrix_p2r()
-        new_rcoord = (realCoord[0], realCoord[1], 1.0)
-        x = np.matmul(inv(self.transformation_matrix), new_rcoord)
-        # self.node.get_logger().info("pixel_value: " + str(x.astype(int)))
-        return x.astype(int)
+
+        x, y = realCoord
+        
+        xtrans = x-self.posePoint[0]
+        ytrans = y-self.posePoint[1]
+        theta = self.angles[2]
+
+        # Apply inverse rotation using the negative of the yaw
+        x_rotated = xtrans * math.cos(-theta) - ytrans * math.sin(-theta)
+        y_rotated = xtrans * math.sin(-theta) + ytrans * math.cos(-theta)
+
+        # Scale (x_rotated, y_rotated) by the inverse of the resolution to get grid coordinates
+        u = int(abs(x_rotated) / self.resolution)
+        v = int(y_rotated / self.resolution)
+
+        return (u, v) 
     
+    def distance_point_to_line_segment(self, point, line_segment):
+        # Unpack points of the line segment
+        p1, p2 = line_segment
+        
+        # Convert points to numpy arrays
+        p1 = np.array(p1)
+        p2 = np.array(p2)
+        point = np.array(point)
+        
+        # Vector representing the line segment
+        v = p2 - p1
+        
+        # Vector from p1 to the point
+        w = point - p1
+        
+        # Projection of w onto v
+        c1 = np.dot(w, v)
+        c2 = np.dot(v, v)
+        
+        # Distance from the point to the line segment
+        b = c1 / c2
+        if b < 0:
+            return np.linalg.norm(point - p1)
+        elif b > 1:
+            return np.linalg.norm(point - p2)
+        else:
+            pb = p1 + b * v
+            return np.linalg.norm(point - pb)
+
+
+    def closest_line_segment(self, point, line_segments):
+        min_distance = float('inf')
+        closest_segment = None
+
+        # Iterate through each consecutive pair of points
+        for i in range(len(line_segments) - 1):
+            segment_start = line_segments[i]
+            segment_end = line_segments[i + 1]
+            
+            # Form line segment from consecutive points
+            segment = (segment_start, segment_end)
+            
+            # Calculate distance from point to line segment
+            distance = self.distance_point_to_line_segment(point, segment)
+            
+            # Update closest segment if needed
+            if distance < min_distance:
+                min_distance = distance
+                closest_segment = segment
+
+        return (closest_segment, min_distance)
+
+    def perpendicular_line_through_point(self, point, line_segments):
+        closest_segment = self.closest_line_segment(point, line_segments)[0]
+
+        # Calculate the equation of the perpendicular line
+        p1, p2 = closest_segment
+        x1, y1 = p1
+        x2, y2 = p2
+
+        # Calculate the slope of the closest lane segment
+        if x2 - x1 == 0:  # Handling the case where the line segment is vertical
+            slope_lane = float('inf')  # Assigning infinity as slope
+        else:
+            slope_lane = (y2 - y1) / (x2 - x1)
+
+        # Calculate the perpendicular slope
+        if slope_lane == 0:  # Handling the case where the lane segment is horizontal
+            slope_perpendicular = float('inf')  # Assigning infinity as slope
+        else:
+            slope_perpendicular = -1 / slope_lane
+
+        # Calculate the intercept of the perpendicular line with the lane segment
+        intercept = point[1] - slope_perpendicular * point[0]
+
+        return slope_perpendicular, intercept
+
+    
+    def point_to_segment_distance(self, point, segment_start, segment_end):
+        x0, y0 = point
+        x1, y1 = segment_start
+        x2, y2 = segment_end
+        dx = x2 - x1
+        dy = y2 - y1
+        if dx == dy == 0:  # Check if the segment is just a point
+            return math.hypot(x1 - x0, y1 - y0)
+
+        t = ((x0 - x1) * dx + (y0 - y1) * dy) / (dx * dx + dy * dy)
+
+        if t < 0:
+            closest_point = x1, y1
+        elif t > 1:
+            closest_point = x2, y2
+        else:
+            closest_point = x1 + t * dx, y1 + t * dy
+
+        return math.hypot(x0 - closest_point[0], y0 - closest_point[1])
+
+    def point_not_within_distance(self, path, point, distance_threshold):
+        for i in range(len(path) - 1):
+            segment_start = path[i]
+            segment_end = path[i + 1]
+            # for point in points:
+            distance = self.point_to_segment_distance(point, segment_start, segment_end)
+            if distance <= distance_threshold:
+                return False  # If point is not within the threshold, return True
+        return True
+    
+
+    def perpendicular_orientation_at_point(self, point):
+        closest_segment = self.closest_line_segment(point, self.lanes)[0]
+
+        # Calculate the closest point on the line segment to the given point
+        p1, p2 = closest_segment
+        x1, y1 = p1
+        x2, y2 = p2
+
+        # Calculate the vector from the given point to the closest point on the line segment
+        dx = x2 - x1
+        dy = y2 - y1
+        vx = point[0] - x1
+        vy = point[1] - y1
+
+        # Calculate the length of the line segment
+        segment_length = math.sqrt(dx * dx + dy * dy)
+
+        # Project the point onto the line segment
+        t = max(0, min((vx * dx + vy * dy) / (segment_length * segment_length), 1))
+        closest_point_x = x1 + t * dx
+        closest_point_y = y1 + t * dy
+
+        # Calculate the orientation angle from the given point to the closest point on the line segment
+        orientation = math.atan2(closest_point_y - point[1], closest_point_x - point[0]) - math.pi / 2
+
+        return orientation
+
+
     def get_pixel(self, u, v):
         return self.data[v][u]
+    
+    def bfs(self, start_point, end_point):
+
+        start_point = self.discretization(start_point[0], start_point[1])
+        end_point = self.discretization(end_point.x, end_point.y)
+        # start_point = self.discretization(11.094404220581055, -1.1191177368164062)
+        # end_point = self.discretization(-16.592947006225586, 25.76806640625)
+
+        visited = set(start_point) 
+        queue = [(start_point, [start_point])]
+        
+        while queue:
+            node, path = queue.pop(0)
+            if node == end_point:
+                return path   
+                
+            for neighbor in self.get_neighbors(node[0], node[1]):
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    queue.append((neighbor, path + [neighbor]))
 
     def discretization(self, x, y):
-        return (math.floor(x) + 0.5, math.floor(y) + 0.5)
+        rounded_x = round(x * 2) / 2  # Round x to the nearest multiple of 0.5
+        rounded_y = round(y * 2) / 2  # Round y to the nearest multiple of 0.5
+        return (rounded_x, rounded_y)
+
+
+    # def get_neighbors(self, x, y):
+    #     neighbors = []
+    #     directions = [(0, 0.5), (0.5, 0), (0, -0.5), (-0.5, 0), (-0.5, 0.5), (0.5, 0.5), (0.5, -0.5), (-0.5, -0.5)]
+    #     for dx, dy in directions:
+    #         nx, ny = x + dx, y + dy
+    #         u, v = self.real_to_pixel((nx, ny))
+    #         if (-self.width <= u and u < self.width) and (-self.height <= v and v < self.height) and (self.get_pixel(u, v) == 0): # self.point_not_within_distance(self.lanes, (nx, ny), 0.7):
+    #             neighbors.append((nx, ny))
+
+    #     return neighbors
 
     def get_neighbors(self, x, y):
         neighbors = []
-        directions = [(-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0)]
+        directions = [(0, 0.5), (0.5, 0), (0, -0.5), (-0.5, 0), (-0.5, 0.5), (0.5, 0.5), (0.5, -0.5), (-0.5, -0.5)]
+        
+        # Get the perpendicular orientation at the current point
+        orientation = self.perpendicular_orientation_at_point((x, y))
+        
+        # Convert orientation to degrees for comparison
+        orientation_degrees = math.degrees(orientation)
+        
+        # Define the acceptable orientation range (e.g., +/- 45 degrees from perpendicular)
+        orientation_range = 45
 
+
+        # if you are far enough from the lane, then you can consider all neighbors
+        
+        # this is to account for points that may be in corners/edges that will require 
+        # the car to move in a direction that is not directly perpendicular to the lane
+
+        min_distance = self.closest_line_segment((x, y), self.lanes)[1] 
+        
         for dx, dy in directions:
-            # nx, ny = x + dx/2, y + dy/2
-            nx, ny = x + dx/2, y + dy/2
-            # self.node.get_logger().info("NX, NY: " + str((nx, ny)))
-            u, v, b = self.real_to_pixel((nx, ny))
-            # self.node.get_logger().info("U, V: " + str((u, v)))
-            # self.node.get_logger().info("Free" if self.get_pixel(u, v) == 0 else "Occupied")
-            # if True:
-            if self.get_pixel(u, v) == 0:
-                move_cost = 14 if dx != 0 and dy != 0 else 10  # Diagonal moves cost 14, others cost 10
-                neighbors.append((nx, ny, move_cost))
+
+            if min_distance < 1:
+                # Calculate the orientation of the current direction
+                direction_orientation = math.atan2(dy, dx)
+                
+                # Convert direction orientation to degrees for comparison
+                direction_orientation_degrees = math.degrees(direction_orientation)
+                
+                # Check if the direction is within the acceptable range of orientations
+                if abs(direction_orientation_degrees - orientation_degrees) <= orientation_range:
+                    nx, ny = x + dx, y + dy
+                    u, v = self.real_to_pixel((nx, ny))
+                    if (-self.width <= u < self.width) and (-self.height <= v < self.height): # and (self.get_pixel(u, v) == 0):
+                        neighbors.append((nx, ny))
+            else:
+                nx, ny = x + dx, y + dy
+                u, v = self.real_to_pixel((nx, ny))
+                if (-self.width <= u and u < self.width) and (-self.height <= v and v < self.height) and (self.get_pixel(u, v) == 0): # self.point_not_within_distance(self.lanes, (nx, ny), 0.7):
+                    neighbors.append((nx, ny))
+
         return neighbors
-    
-    def heuristic(self, a, b):
-        (x1, y1) = a
-        (x2, y2) = b
-        return math.sqrt((x1 - x2)**2 + (y1 - y2)**2) * 10
-    
-    def a_star(self, start, end):
-        self.data = self.dilate_map()
-        start = self.discretization(start[0], start[1])
-        end = self.discretization(end[0], end[1])
-
-        #start = self.discretization(11.094404220581055, -1.1191177368164062)
-        #end = self.discretization(-16.592947006225586, 25.76806640625)
-
-        frontier = [(0, start)]
-        came_from = {start: None}
-        cost_so_far = {start: 0}
-
-        while frontier:
-        # Sort and pop the lowest cost item
-            frontier.sort(key=lambda x: x[0])
-            current = frontier.pop(0)[1]
-
-            if current == end:
-                break
-
-            # Correctly unpack all three values returned from get_neighbors
-            # self.node.get_logger().info("Neighbors: " + str(self.get_neighbors(current[0], current[1])))
-            for nx, ny, move_cost in self.get_neighbors(current[0], current[1]):
-                next_pos = (nx, ny)
-                new_cost = cost_so_far[current] + move_cost
-                if next_pos not in cost_so_far or new_cost < cost_so_far[next_pos]:
-                    cost_so_far[next_pos] = new_cost
-                    priority = new_cost + self.heuristic(end, next_pos)
-                    frontier.append((priority, next_pos))
-                    came_from[next_pos] = current
-
-        return self.reconstruct_path(came_from, start, end)
-
-    def reconstruct_path(self, came_from, start, end):
-        current = end
-        path = []
-        while current != start:
-            path.append(current)
-            current = came_from.get(current)
-        path.append(start)
-        path.reverse()
-        return path
